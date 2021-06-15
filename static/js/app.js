9f18bbcaedea9c81a9939eea71d2472dafd046d4
Travbz <78450169+Travbz@users.noreply.github.com>
2021-06-14 19:17:28 -0600

    mod

--- static/js/app.js
+++ static/js/app.js
@@ -1,45 +1,10 @@
-
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // Console.log the UFO data from data.js
 console.log(data);
 
-// Step 1: Loop Through `data` and console.log each UFO report object
-data.forEach(function(ufoReport) {
-  console.log(ufoReport);
-});
-
-// Step 2:  Use d3 to append one table row `tr` for each ufo report object
-data.forEach(function(ufoReport) {
-  console.log(ufoReport);
-  var row = tbody.append("tr");
-});
-
-// Step 3:  Use `Object.entries` to console.log each ufo report value
-data.forEach(function(ufoReport) {
-  console.log(ufoReport);
-  var row = tbody.append("tr");
-
-  Object.entries(ufoReport).forEach(function([key, value]) {
-    console.log(key, value);
-  });
-});
-
-// Step 4: Use d3 to append 1 cell per UFO report value (Datetime, City, State, Country, Shape, Duration, Comments)
-data.forEach(function(ufoReport) {
-  console.log(ufoReport);
-  var row = tbody.append("tr");
-
-  Object.entries(ufoReport).forEach(function([key, value]) {
-    console.log(key, value);
-    // Append a cell to the row for each value
-    // in the UFO report object
-    var cell = row.append("td");
-  });
-});
-
-// Step 5: Use d3 to update each cell's text with
+// Step 1: Use d3 to update each cell's text with
 // UFO report values (Datetime, City, State, Country, Shape, Duration, Comments)
 data.forEach(function(ufoReport) {
   console.log(ufoReport);
@@ -53,7 +18,26 @@ data.forEach(function(ufoReport) {
   });
 });
 
+// filterTable(data);
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = data.getElementById("form-control");
+  filter = input.value.toUpperCase();
+  table = data.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
 
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
 
-
-// YOUR CODE HERE!
