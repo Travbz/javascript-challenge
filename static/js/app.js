1895ab01f188dbfc1199e9469408ec22c7c1e732
Travbz <78450169+Travbz@users.noreply.github.com>
2021-06-19 11:50:42 -0600

    final

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,11 @@
 function loadHTML(loadTableData) {
   var tbody = d3.select("tbody");
+  console.log(loadTableData);
   loadTableData.forEach(function(ufoObject) {
+      console.log(ufoObject);
       var row = tbody.append("tr");
       Object.entries(ufoObject).forEach(function([key, value]) {
+          console.log(key, value);
           var cell = row.append("td");
           cell.text(value);
       });
@@ -22,8 +25,7 @@ searchBar.addEventListener("keyup", (e) => {
           reports.city.toLowerCase().includes(searchString) ||
           reports.state.toLowerCase().includes(searchString) ||
           reports.country.toLowerCase().includes(searchString) ||
-          reports.shape.toLowerCase().includes(searchString) ||
-          reports.comments.toLowerCase().includes(searchString)
+          reports.shape.toLowerCase().includes(searchString) 
       );
   });
   loadHTML(displayResult);
